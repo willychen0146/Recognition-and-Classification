c2b4cbdc2da182404ce68c833f8719d8fbf58c05
NoMercySusie <willychen0146@gmail.com>
2023-10-27 01:25:26 +0800

    more detail about README

---
+++ build.sh
@@ -0,0 +1,13 @@
+conda create -n BoW_CNN python=3.6
+conda activate BoW_CNN
+pip install numpy
+pip install cyvlfeat
+pip install matplotlib
+pip install Pillow
+pip install scipy
+pip install opencv-python
+pip install sklearn
+pip install torch
+pip install torchvision
+pip install tdqm
+pip install gdown
